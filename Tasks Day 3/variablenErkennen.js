c4ec704a17414fa4b605726cc96c96c51fa97907
Hannes Bröhl <hannes.broehl@icloud.com>
2023-01-15 15:27:38 +0100

    ermitteln v. Variablen (nützlich)

---
+++ Tasks Day 3/variablenErkennen.js
@@ -0,0 +1,27 @@
+let str = 'Hello';
+let nr = 7;
+let bigNr = 12345678901234;
+let bool = true;
+let sym = Symbol("unique");
+let undef = undefined;
+let unknown = null;
+//
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log("1 str ist folgende Art einer Variablen ... ", typeof str);
+console.log("2 nr", typeof nr);
+console.log("3 bigNr", typeof bigNr);
+console.log("4 bool", typeof bool);
+console.log("5 sym", typeof sym);
+console.log("6 undef", typeof undef);
+console.log("7 unknown", typeof null);
+//
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof val1);
+console.log(typeof val2);
+console.log(typeof myNum);
