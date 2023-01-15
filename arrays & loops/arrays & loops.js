bb1a418824c50d5d2c3ec4195c19b8f350a1fbc1
Hannes Bröhl <hannes.broehl@icloud.com>
2023-01-15 17:53:47 +0100

    arrays & loops

---
+++ arrays & loops/arrays & loops.js
@@ -0,0 +1,80 @@
+// Array's
+
+
+// Array wird über eckige Klammern erzeugt!
+// Array ist eine Liste mit beliebig vielen Einträgen
+
+// const arr = [];
+// const arr1 = ['a'];
+// const arr2 = ['a', 'b', 'c'];
+
+// console.log(arr);
+// console.log(arr1);
+// console.log(arr2);
+
+
+// const arr = ['Tim', 'Lars', 'Jens', 'Anja'];
+// console.log(arr.length); // anzähl der Einträge im Array!
+// console.log(arr[0]); // index Zugriff
+// console.log(arr[1]); // index Zugriff
+// console.log(arr[2]); // index Zugriff
+// console.log(arr[3]); // index Zugriff
+
+// const arr = [1, false, undefined, 'Tim', 1.111, null, [1,2,3]];
+// console.log(arr.length);
+
+// // Index beginnt bei 0 -- Zero Based Index!
+
+// const arr2 = arr[6];
+// console.log(arr2.length);
+// console.log(arr2[1]);
+
+// //direkter Zugriff auf Array innerhalb eines Arrays
+// console.log(arr[6][1]);
+
+
+
+// Loops
+// es gibt (for, while, do-while)
+
+// for-schleife: führe einen bestimmten Code n mal aus!
+
+// for (initialisierung; condition; afterthought)
+// i = iterator (= man macht etwas ein paar mal!)
+// for(let i = 0; i < 5; i = i+1) {
+//     console.log('Hallo')
+//     console.log('Tschüß');
+// }
+
+// //...............0.......1.......2.......3......4........5.......
+// const names = ['Anja', 'Tim', 'Jens', 'Timo', 'Lars', 'Nico'];
+
+// for(let i = 0; i < names.length/*6*/; i = i+1) {
+//     //console.log('i: ' + i);
+//     console.log('Hallo ' + names[i]);
+// }
+
+
+// const sheeps = [true, true, false, true, false, true, true];
+// let countSheep = 0;
+// // i++ ---> i = i+1
+// for(let i = 0; i < sheeps.length; i++){
+//     if(sheeps[i] === true) {
+//         countSheep = countSheep + 1;
+//     }
+// }
+
+// console.log(countSheep);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
